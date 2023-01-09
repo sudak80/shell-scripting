ca6ce2e4e2be892971f2eeca1c1e76cc300bba01
Sudarsana K <sudak80@gmail.com>
2023-01-09 18:55:43 +0530

    shell script topics added.

--- 02-print.sh
+++ 02-print.sh
@@ -1,2 +1,17 @@
 
 echo "** Welcome to Upskill journey **"
+
+# To use color code we have to use echo -e "\e[COLmMESSAGE\[0m"
+# Color COde Syntax : echo -e "\e[COLmMESSAGE\e[0m"
+# Red       - 31 - echo -e "\e[31mMESSAGE\e[0m"
+# Green     - 32 - echo -e "\e[32mMESSAGE\e[0m"
+# Yellow    - 33 - echo -e "\e[33mMESSAGE\e[0m"
+# Blue      - 34 - echo -e "\e[34mMESSAGE\e[0m"
+# Magenta   - 35 - echo -e "\e[35mMESSAGE\e[0m"
+# Cyan      - 36 - echo -e "\e[36mMESSAGE\e[0m"
+
+echo -e "\e[31mMESSAGE\e[0m"
+echo -e "\e[32mMESSAGE\e[0m"
+
+
+# https://misc.flogisoft.com/bash/tip_colors_and_formatting
