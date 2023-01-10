372fc52166cfd2be2c4438e2f21f79d3029d35f4
Sudarsana K <sudak80@gmail.com>
2023-01-10 07:01:13 +0530

    commit

--- roboshop-shell/01.frontend.sh
+++ roboshop-shell/01.frontend.sh
@@ -1,4 +1,5 @@
 #Hear we are going automate frontend server configuration...
+current_user=$(pwd)
 
 yum install nginx -y
 
@@ -12,6 +13,6 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.z
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-cp /files/roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp ${current_user}/files/roboshop.conf /etc/nginx/default.d/roboshop.conf
 
 systemctl restart nginx
