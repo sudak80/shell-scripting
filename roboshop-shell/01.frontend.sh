b798ca8331edffc5059d10ee5e7581486e09d1fe
Sudarsana K <sudak80@gmail.com>
2023-02-07 15:50:32 +0530

    commit - freshgreens

--- roboshop-shell/01.frontend.sh
+++ roboshop-shell/01.frontend.sh
@@ -8,11 +8,10 @@ systemctl start nginx
 
 rm -rf /usr/share/nginx/html/*
 
-#curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-curl -o /usr/share/nginx/html/ https://github.com/skfreshgreens/frontend.git
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
 cd /usr/share/nginx/html
-#unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip
 
 cp ${config_file_location}/files/roboshop.conf /etc/nginx/default.d/roboshop.conf
 
