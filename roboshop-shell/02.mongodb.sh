2f222b8b4c615222cf4cb93f4a884af69143a011
Sudarsana K <sudak80@gmail.com>
2023-02-07 15:58:49 +0530

    commit - freshgreens

--- roboshop-shell/02.mongodb.sh
+++ roboshop-shell/02.mongodb.sh
@@ -2,3 +2,13 @@
 config_file_location=$(pwd)
 
 cp ${config_file_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
+
+yum install mongodb-org -y
+
+systemctl enable mongod
+systemctl start mongod
+
+sed -i 's/27.0.0.1/0.0.0.0/g' /etc/mongod.conf
+
+
+systemctl restart mongod
