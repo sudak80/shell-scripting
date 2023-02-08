f5edfc1ef755b3278d22771ab89e298ddf484bef
Sudarsana K <sudak80@gmail.com>
2023-02-08 10:48:24 +0530

    added catalogue service

--- roboshop-shell/03.catalog.sh
+++ roboshop-shell/03.catalog.sh
@@ -3,28 +3,20 @@ config_file_location=$(pwd)
 set -e
 
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-
 yum install nodejs -y
-
 useradd roboshop
-
-mkdir /app
+mkdir -p /app
 
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
 unzip /tmp/catalogue.zip
-
-cd /app
 npm install
 
 cp ${config_file_location}/files/catalogue.service /etc/systemd/system/catalogue.service
-
 cp ${config_file_location}/files/mongodb.repo /etc/yum.repos.d/mongo.repo
 
 yum install mongodb-org-shell -y
-
 mongo --host localhost </app/schema/catalogue.js
-
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
