8d4c42901cc511f1cc64ef8c71383bf7eec32490
Sudarsana K <sudak80@gmail.com>
2023-02-23 12:04:57 +0530

    exit message updated

--- roboshop-shell/02.mongodb.sh
+++ roboshop-shell/02.mongodb.sh
@@ -1,13 +1,49 @@
 # mongodb configuration details
 config_file_location=$(pwd)
-set -e    # This will stop wherver issue occurs and will not proceed further.
+#set -e    # This will stop wherver issue occurs and will not proceed further.
+LOG=/tmp/roboshop.log
 
-cp ${config_file_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
+echo -e "\e[35m Creating mongodb repo\e[0m"
+cp ${config_file_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${LOG}
+if [ $? -eq 0 ]
+then
+  echo "SUCCESS"
+else
+  echo "FAILURE"
+fi
 
-yum install mongodb-org -y
+echo -e "\e[35m Install mongodb\e[0m"
+yum install mongodb-org -y &>>${LOG}
+if [ $? -eq 0 ]
+then
+  echo "SUCCESS"
+else
+  echo "FAILURE"
+fi
 
-sed -i 's/10.0.0.0/0.0.0.0/g' /etc/mongod.conf
+echo -e "\e[35m Allowing mongodb to accept traffic from anywhere (incoming)\e[0m"
+sed -i 's/10.0.0.0/0.0.0.0/g' /etc/mongod.conf &>>${LOG}
+if [ $? -eq 0 ]
+then
+  echo "SUCCESS"
+else
+  echo "FAILURE"
+fi
 
-systemctl enable mongod
-systemctl start mongod
-systemctl restart mongod
+echo -e "\e[35m Enable mongodb\e[0m"
+systemctl enable mongod &>>${LOG}
+if [ $? -eq 0 ]
+then
+  echo "SUCCESS"
+else
+  echo "FAILURE"
+fi
+
+echo -e "\e[35m Starting mongodb service\e[0m"
+systemctl start mongod &>>${LOG}
+if [ $? -eq 0 ]
+then
+  echo "SUCCESS"
+else
+  echo "FAILURE"
+fi
