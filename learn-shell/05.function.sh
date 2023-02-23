9595b8bea3f9b75e405e88a43ea59ba786f6320a
Sudarsana K <sudak80@gmail.com>
2023-02-23 13:12:34 +0530

    exit message updated

---
+++ learn-shell/05.function.sh
@@ -0,0 +1,12 @@
+#	Functions are like variables
+#	Variable – When we assign a name to a set of data then it is a variable.
+#	Function – When we assign a name to a set of commands, it is a variable.
+
+# Declaring Function
+example() {
+  echo I am a example function
+}
+
+# How to call a Function
+example
+
